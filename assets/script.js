9e2da90cb13e839cbb66a267b28a63ef47940669
raikiran07 <raikiran925@gmail.com>
2022-07-19 18:13:08 +0530

    quiz api is used to get the questions from the internet

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,85 @@
-const questions = [
-  {
-    questionText: "Commonly used data types DO NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    answer: "3. alerts",
-  },
-  {
-    questionText: "Arrays in JavaScript can be used to store ______.",
-    options: [
-      "1. numbers and strings",
-      "2. other arrays",
-      "3. booleans",
-      "4. all of the above",
-    ],
-    answer: "4. all of the above",
-  },
-  {
-    questionText:
-      "String values must be enclosed within _____ when being assigned to variables.",
-    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
-    answer: "3. quotes",
-  },
-  {
-    questionText:
-      "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: [
-      "1. JavaScript",
-      "2. terminal/bash",
-      "3. for loops",
-      "4. console.log",
-    ],
-    answer: "4. console.log",
-  },
-  {
-    questionText:
-      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
-    options: ["1. break", "2. stop", "3. halt", "4. exit"],
-    answer: "1. break",
-  },
-];
+//calling api to get the questions
+const questions = [];
+function getQuestion(){
+  
+  fetch("https://opentdb.com/api.php?amount=20&category=18&difficulty=medium").then(res=>res.json()).then(data=>{
+        
+    
+    
+    
+    const details = data['results'];
+    //question
+    //options - incorrect questions slice correct questions
+    //correct-option - correct answer
+    for(let i=0;i<20;i++){
+      const items = {questionText:"",options:[],answer:""};
+        let optionsList = details[i]['incorrect_answers'];
+        items.answer = details[i]['correct_answer'];
+        const question = details[i]['question'];
+        items.questionText = question;
+        optionsList.splice(Math.floor(Math.random() * (details[i]['incorrect_answers'].length + 1)), 0, details[i]['correct_answer']);
+        items.options = optionsList;
+        questions.push(items);
+        // console.log(optionsList);
+
+    }
+
+   
+    
+    
+})
+
+}
+
+console.log(questions)
+
+
+//calling the function to store the question before displaying it
+getQuestion();
+
+
+
+
+// const questions = [
+//   {
+//     questionText: "Commonly used data types DO NOT include:",
+//     options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+//     answer: "3. alerts",
+//   },
+//   {
+//     questionText: "Arrays in JavaScript can be used to store ______.",
+//     options: [
+//       "1. numbers and strings",
+//       "2. other arrays",
+//       "3. booleans",
+//       "4. all of the above",
+//     ],
+//     answer: "4. all of the above",
+//   },
+//   {
+//     questionText:
+//       "String values must be enclosed within _____ when being assigned to variables.",
+//     options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+//     answer: "3. quotes",
+//   },
+//   {
+//     questionText:
+//       "A very useful tool used during development and debugging for printing content to the debugger is:",
+//     options: [
+//       "1. JavaScript",
+//       "2. terminal/bash",
+//       "3. for loops",
+//       "4. console.log",
+//     ],
+//     answer: "4. console.log",
+//   },
+//   {
+//     questionText:
+//       "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+//     options: ["1. break", "2. stop", "3. halt", "4. exit"],
+//     answer: "1. break",
+//   },
+// ];
 
 
 
@@ -122,7 +164,9 @@ function start(){
      timer = setInterval(function(){
        
         if(counter<10){
+            // showTime = '0'+counter;
             time.innerHTML = '0'+counter;
+            time.style.color="red";
         }
         else{
             time.innerHTML = counter;
